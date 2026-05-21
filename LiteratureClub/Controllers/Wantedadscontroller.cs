537203a539476739b3ab26d93c8cf24bb551308f
JudyBambo <judybambo@ymail.com>
2026-05-22 00:17:22 +0200

    Added AdminController.

--- LiteratureClub/Controllers/Wantedadscontroller.cs
+++ LiteratureClub/Controllers/Wantedadscontroller.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace BookSwap.Controllers
+namespace LiteratureClub.Controllers
 {
     public class WantedAdsController : Controller
     {
